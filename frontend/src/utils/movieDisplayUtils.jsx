c253badffc8c2c36b90ee828c30df4eaf57b267e
Timothy Chen <timothy.chen188@gmail.com>
2025-04-06 09:45:46 +0100

    Refactoring Utils and Style updates
    Made a few styling adjustments to the movie details [id].astro page.
    
    Refactored the popularity bar, and also converted the utils file to jsx to allow for returning html elements instead of strings.

--- frontend/src/utils/movieDisplayUtils.jsx
+++ frontend/src/utils/movieDisplayUtils.jsx
@@ -24,3 +24,21 @@ export function formatVoteBadge(vote_average){
   if (vote >= 5) return 'bg-yellow-500/60 text-white';
   return 'bg-red-500/50 text-white';
 }
+
+export function formatPopularityBar(popularity){
+  if(!popularity) return null;
+
+  const width = Math.min(100, popularity / 10); // cap at 100%
+
+  return (
+    <div className="space-y-1">
+      <div className="text-xs text-slate-500 font-light tracking-wider">POPULARITY:</div>  
+      <p className="text-xs text-slate-300 font-light tracking-wide">{Math.floor(width)}%</p>
+      <div className="w-full h-1 bg-slate-800 rounded-full overflow-hidden">
+        <div className="h-full bg-gradient-to-r from-cyan-200 to-blue-500" style={{ width: `${width}%`}}></div>
+      </div>
+      
+
+    </div>
+  );
+}
