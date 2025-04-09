9338b74c0d9fc2f99a2c2be9bd7469753e71bc71
Timothy Chen <timothy.chen188@gmail.com>
2025-04-09 21:32:37 +0100

    Updated MoviePoster to Key Poster
    Also updated the MoviePoster component to display the key poster for each movie

--- frontend/src/components/MoviePoster.jsx
+++ frontend/src/components/MoviePoster.jsx
@@ -1,16 +1,6 @@
 import React from "react";
 
 export default function MoviePoster({ movie }) {
-  // Handle null, single entry, or multiple poster_url values
-  const posterUrls = movie.poster_url
-    ? movie.poster_url.includes(",")
-      ? movie.poster_url
-          .split(",")
-          .map((url) => url.trim())
-          .slice(0, 10)
-      : [movie.poster_url.trim()] // Wrap single entry in an array
-    : [];
-
   return (
     <>
       {/* Movie Poster */}
@@ -18,7 +8,7 @@ export default function MoviePoster({ movie }) {
         <div className="block overflow-hidden">
           <img
             className="w-full rounded-md shadow-xl"
-            src={posterUrls[0].trim()} // Use first URL and trim
+            src={movie.keyposter_url}
             alt={`${movie.title} Poster`}
           />
         </div>
