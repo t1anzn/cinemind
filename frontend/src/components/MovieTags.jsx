b6589e13951a03021762519bb8c53f3174ca4422
Timothy Chen <timothy.chen188@gmail.com>
2025-04-10 23:58:17 +0100

    Clearing console errors

--- frontend/src/components/MovieTags.jsx
+++ frontend/src/components/MovieTags.jsx
@@ -9,7 +9,10 @@ export default function MovieTags({ movie }) {
         <div class="flex flex-wrap space-x-2 space-y-2 mb-4">
           {movie.genres &&
             movie.genres.map((genre, index) => (
-              <span class="inline-flex items-center justify-center bg-gradient-to-b from-blue-600/20 to-transparent text-white text-xs font-semibold py-1 px-3 rounded-xl border-1 border-blue-400/50 h-8 min-h-[32px] hover:bg-cyan-500 transition-all duration-300">
+              <span
+                key={index}
+                class="inline-flex items-center justify-center bg-gradient-to-b from-blue-600/20 to-transparent text-white text-xs font-semibold py-1 px-3 rounded-xl border-1 border-blue-400/50 h-8 min-h-[32px] hover:bg-cyan-500 transition-all duration-300"
+              >
                 {genre}
               </span>
             ))}
@@ -20,7 +23,10 @@ export default function MovieTags({ movie }) {
         <div class="flex flex-wrap space-x-2 space-y-2">
           {movie.keywords &&
             movie.keywords.map((keyword, index) => (
-              <span class="inline-flex items-center justify-center bg-gradient-to-b from-slate-600/50 to-slate-600/20 border border-slate-600/30 text-white tracking-wider text-xs font-light py-1 px-3 rounded-xl h-8 min-h-[32px] hover:bg-slate-300/50 transition-all duration-300">
+              <span
+                key={index}
+                class="inline-flex items-center justify-center bg-gradient-to-b from-slate-600/50 to-slate-600/20 border border-slate-600/30 text-white tracking-wider text-xs font-light py-1 px-3 rounded-xl h-8 min-h-[32px] hover:bg-slate-300/50 transition-all duration-300"
+              >
                 {keyword}
               </span>
             ))}
